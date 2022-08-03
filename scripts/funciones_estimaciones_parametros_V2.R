249da060c82c2abe29089187a98834be36f3508f
jfzeac <jfzeac@gmail.com>
2022-08-03 11:27:22 -0500

    agosto 3

--- scripts/funciones_estimaciones_parametros_V2.R
+++ scripts/funciones_estimaciones_parametros_V2.R
@@ -222,6 +222,7 @@ lst_me_region[[i]] <- me_calculo(N = f_paramGenerales_dominio(str_dominio)$N[i],
 }
 
 df_me <- bind_rows(lst_me_region)
+df_me[[str_dominio]] <- unique(df_encuesta[[str_dominio]])
 df_me
 }
 
@@ -231,3 +232,124 @@ df_me
 #                                str_indicador = "pobreza" ,
 #                                r_indicador = "r_pobreza")
 # 
+
+
+
+
+
+f_indicadores_nacional <- function() {
+  # Número promedio de personas por UPMs
+  
+  df_promPersUPM <- df_encuesta %>% group_by(upm) %>%
+    summarise(nII = n()) %>% ungroup() %>% 
+    summarise(n_dominio = mean(nII)) 
+  # df_encuesta %>% filter(areageo == 1) %>% group_by(upm) %>% 
+  #   summarise(nII = n())
+  # 
+  df_estima_indicadores <- diseno_pobreza %>% 
+    summarize(pobreza = survey_mean(pobreza_monet, na.rm = T, deff = T),
+              pobrezaExtr = survey_mean(pobreza_monet_extrema, na.rm = T, deff = T),
+              NBI = survey_mean(NBI, na.rm = T, deff = T),
+              tasa_desocupacion  = survey_ratio(desocupados, activos, na.rm = T,
+                                                deff = T),
+              tasa_ocupacion = survey_ratio(ocupados, poblacion_EdadTrabajar2,
+                                            na.rm = T,
+                                            deff = T),
+              tasa_participacion = survey_ratio(activos, poblacion_EdadTrabajar2,
+                                                na.rm = T, deff = T),
+              N = survey_total(unos),
+              N_activos = survey_total(activos, na.rm = T, deff = F)
+    )
+  # %>% arrange(!!sym(str_dominio)) %>% left_join(df_promPersUPM) %>% 
+  #   arrange(!!sym(str_dominio))  
+  # 
+  df_estima_indicadores$n_dominio <- df_promPersUPM$n_dominio # El n barra global
+  
+  names(df_estima_indicadores)[names(df_estima_indicadores) == "pobreza"] <- "pobreza_prop"
+  names(df_estima_indicadores)[names(df_estima_indicadores) == "pobrezaExtr"] <- "pobrezaExtr_prop"
+  names(df_estima_indicadores)[names(df_estima_indicadores) == "NBI"] <- "NBI_prop"
+  names(df_estima_indicadores)[names(df_estima_indicadores) == "tasa_desocupacion"] <- "tasa_desocupacion_prop"
+  names(df_estima_indicadores)[names(df_estima_indicadores) == "tasa_ocupacion"] <- "tasa_ocupacion_prop"
+  names(df_estima_indicadores)[names(df_estima_indicadores) == "tasa_participacion"] <- "tasa_participacion_prop"
+  
+  conf <- 0.95
+  df_estima_indicadores <- df_estima_indicadores %>%
+    mutate(pobreza_prop = round(pobreza_prop, 2),
+           pobreza_se = round(pobreza_se, 4),
+           pobreza_me = round(qnorm(1-(1-conf)/2) * pobreza_se, 4),
+           pobreza_mer = round(pobreza_me / pobreza_prop, 4),
+           pobreza_cve = round(100 * pobreza_se / pobreza_prop, 1),
+           pobreza_LI = pobreza_prop - pobreza_me,
+           pobreza_LS = pobreza_prop + pobreza_me,
+           pobreza_deff = round(pobreza_deff, 1),
+           pobreza_rho = (pobreza_deff - 1) / (n_dominio - 1),
+           
+           pobrezaExtr_prop = round(pobrezaExtr_prop, 2),
+           pobrezaExtr_se = round(pobrezaExtr_se, 4),
+           pobrezaExtr_me = round(qnorm(1-(1-conf)/2) * pobrezaExtr_se,4),
+           pobrezaExtr_mer = round(pobrezaExtr_me / pobrezaExtr_prop, 4),
+           pobrezaExtr_cve = round(100 * pobrezaExtr_se / pobrezaExtr_prop, 1),
+           pobrezaExtr_LI = pobrezaExtr_prop - pobrezaExtr_me,
+           pobrezaExtr_LS = pobrezaExtr_prop + pobrezaExtr_me,
+           pobrezaExtr_deff = round(pobrezaExtr_deff, 1),
+           pobrezaExtr_rho = (pobrezaExtr_deff - 1) / (n_dominio - 1),
+           
+           NBI_prop = round(NBI_prop, 2),
+           NBI_se = round(NBI_se, 4),
+           NBI_me = round(qnorm(1-(1-conf)/2) * NBI_se,4),
+           NBI_mer = round(NBI_me / NBI_prop, 4),
+           NBI_cve = round(100 * NBI_se / NBI_prop,1),
+           NBI_LI = NBI_prop - NBI_me,
+           NBI_LS = NBI_prop + NBI_me,
+           NBI_deff = round(NBI_deff, 1),
+           NBI_rho = (NBI_deff - 1) / (n_dominio - 1),
+           
+           tasa_desocupacion_prop = round(tasa_desocupacion_prop, 4),
+           tasa_desocupacion_se = round(tasa_desocupacion_se, 4),
+           tasa_desocupacion_me = round(qnorm(1-(1-conf)/2) * tasa_desocupacion_se,4),
+           tasa_desocupacion_mer = round(tasa_desocupacion_me / tasa_desocupacion_prop, 4),
+           tasa_desocupacion_cve = round(100 * tasa_desocupacion_se / tasa_desocupacion_prop, 2),
+           tasa_desocupacion_LI = tasa_desocupacion_prop - tasa_desocupacion_me,
+           tasa_desocupacion_LS = tasa_desocupacion_prop + tasa_desocupacion_me,
+           tasa_desocupacion_deff = round(tasa_desocupacion_deff, 1),
+           tasa_desocupacion_rho = (tasa_desocupacion_deff - 1) / (n_dominio - 1),
+           
+           tasa_ocupacion_prop = round(tasa_ocupacion_prop, 4),
+           tasa_ocupacion_se = round(tasa_ocupacion_se, 4),
+           tasa_ocupacion_me = round(qnorm(1-(1-conf)/2) * tasa_ocupacion_se,4),
+           tasa_ocupacion_mer = round(tasa_ocupacion_me / tasa_ocupacion_prop, 4),
+           tasa_ocupacion_cve = round(100 * tasa_ocupacion_se / tasa_ocupacion_prop, 2),
+           tasa_ocupacion_LI = tasa_ocupacion_prop - tasa_ocupacion_me,
+           tasa_ocupacion_LS = tasa_ocupacion_prop + tasa_ocupacion_me,
+           tasa_ocupacion_deff = round(tasa_ocupacion_deff, 1),
+           tasa_ocupacion_rho = (tasa_ocupacion_deff - 1) / (n_dominio - 1),
+           
+           tasa_participacion_prop = round(tasa_participacion_prop, 4),
+           tasa_participacion_se = round(tasa_participacion_se, 4),
+           tasa_participacion_me = round(qnorm(1-(1-conf)/2) * tasa_participacion_se,4),
+           tasa_participacion_mer = round(tasa_participacion_me / tasa_participacion_prop, 4),
+           tasa_participacion_cve = round(100 * tasa_participacion_se / tasa_participacion_prop, 2),
+           tasa_participacion_LI = tasa_participacion_prop - tasa_participacion_me,
+           tasa_participacion_LS = tasa_participacion_prop + tasa_participacion_me,
+           tasa_participacion_deff = round(tasa_participacion_deff, 1),
+           tasa_participacion_rho = (tasa_participacion_deff - 1) / (n_dominio - 1)
+           
+    )  %>%
+    select(
+      pobreza_prop, pobreza_se, pobreza_me, pobreza_mer,
+      pobreza_cve, pobreza_LI, pobreza_LS, pobreza_deff, pobreza_rho,
+      pobrezaExtr_prop, pobrezaExtr_se, pobrezaExtr_me, pobrezaExtr_mer,
+      pobrezaExtr_cve, pobrezaExtr_LI, pobrezaExtr_LS, pobrezaExtr_deff, pobrezaExtr_rho,
+      NBI_prop, NBI_se, NBI_me, NBI_mer,
+      NBI_cve, NBI_LI, NBI_LS, NBI_deff, NBI_rho,
+      tasa_desocupacion_prop, tasa_desocupacion_se, tasa_desocupacion_me, tasa_desocupacion_mer,
+      tasa_desocupacion_cve, tasa_desocupacion_LI, tasa_desocupacion_LS, tasa_desocupacion_deff, tasa_desocupacion_rho,
+      tasa_ocupacion_prop, tasa_ocupacion_se, tasa_ocupacion_me, tasa_ocupacion_mer,
+      tasa_ocupacion_cve, tasa_ocupacion_LI, tasa_ocupacion_LS, tasa_ocupacion_deff, tasa_ocupacion_rho,
+      tasa_participacion_prop, tasa_participacion_se, tasa_participacion_me, tasa_participacion_mer,
+      tasa_participacion_cve, tasa_participacion_LI, tasa_participacion_LS, tasa_participacion_deff, 
+      tasa_participacion_rho, N_activos, N, n_dominio)
+  df_estima_indicadores
+  
+}
+
